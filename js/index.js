ff041fbfb2b905d6592382def61f518f196ac9b7
habibur5313 <habiburmamun313@gmail.com>
2024-09-24 02:57:23 +0600

    sixth-commit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+const transectionHistory = document.getElementById('transection-history')
 // blog
 document.getElementById('blog').addEventListener('click',function(){
   window.location.href = 'blog.html'
@@ -5,10 +6,10 @@ document.getElementById('blog').addEventListener('click',function(){
 // noakhali flood section
 document.getElementById('noakhali-donation-btn').addEventListener('click',function(){
                     const currentBalance = parseFloat(document.getElementById('current-balance').innerText)
-                    const noakhaliDonationAmount = parseFloat(document.getElementById('noakhali-donation-input').value)
+                   const noakhaliDonationAmount = parseFloat(document.getElementById('noakhali-donation-input').value)
                     const noakhaliDonationBalance = parseFloat(document.getElementById('noakhali-donation-balance').innerText)
-                    if(noakhaliDonationAmount < 0 || currentBalance < noakhaliDonationAmount){
-                                        alert('Invalid Amount')
+                   if(noakhaliDonationAmount < 0 || currentBalance < noakhaliDonationAmount || isNaN(noakhaliDonationAmount)){
+                                        alert('Invalid Donation Amount')
                                         const modal = document.getElementById('my_modal_1')
                                           modal.classList.add('hidden')
                                         return
@@ -16,9 +17,16 @@ document.getElementById('noakhali-donation-btn').addEventListener('click',functi
                    
                     else{
                     document.getElementById('noakhali-donation-balance').innerText =  noakhaliDonationAmount + noakhaliDonationBalance
-                    document.getElementById('current-balance').innerText =  currentBalance - noakhaliDonationAmount
+                    document.getElementById('current-balance').innerText =  currentBalance - noakhaliDonationAmount;
                     const modal = document.getElementById('my_modal_1')
                     modal.classList.remove('hidden')
+                  const div = document.createElement('div');
+                  div.className = 'border p-3 rounded-lg'
+                  div.innerHTML = `
+                  <p class= "text-gray-500 font-extrabold">${noakhaliDonationAmount} TK Donation for Flood at Noakhali, Bangladesh The recent floods in Noakhali have caused.</p>
+                  <p class= "text-gray-500 font-extrabold">${new Date().toLocaleString()}</p>
+                  `
+                  transectionHistory.appendChild(div)
                    
                     }
                     
@@ -29,16 +37,23 @@ document.getElementById('feni-donation-btn').addEventListener('click',function()
                     const currentBalance = parseFloat(document.getElementById('current-balance').innerText)
                     const feniDonationAmount = parseFloat(document.getElementById('feni-donation-input').value)
                     const feniDonationBalance = parseFloat(document.getElementById('feni-donation-balance').innerText)
-                    if(feniDonationAmount < 0 || currentBalance < feniDonationAmount){
+                    if(feniDonationAmount < 0 || currentBalance < feniDonationAmount || isNaN(feniDonationAmount)){
                       const modal = document.getElementById('my_modal_2')
                       modal.classList.add('hidden')
-                                        alert('Invalid Amount')
+                                        alert('Invalid Donation Amount')
                                         return
                     }
                   else { document.getElementById('feni-donation-balance').innerText =  feniDonationAmount + feniDonationBalance
                     document.getElementById('current-balance').innerText =  currentBalance - feniDonationAmount
                     const modal = document.getElementById('my_modal_2')
                     modal.classList.remove('hidden')
+                    const div = document.createElement('div');
+                    div.className = 'border p-3 rounded-lg'
+                    div.innerHTML = `
+                    <p class= "text-gray-500 font-extrabold">${feniDonationAmount} TK Donation for Flood at Feni, Bangladesh The recent floods in Feni have caused.</p>
+                    <p class= "text-gray-500 font-extrabold">${new Date().toLocaleString()}</p>
+                    `
+                    transectionHistory.appendChild(div)
                   }
 
 })
@@ -48,17 +63,27 @@ document.getElementById('quata-donation-btn').addEventListener('click',function(
                     const currentBalance = parseFloat(document.getElementById('current-balance').innerText)
                     const quataDonationAmount = parseFloat(document.getElementById('quata-donation-input').value)
                     const quataDonationBalance = parseFloat(document.getElementById('quata-donation-balance').innerText)
-                    if(quataDonationAmount < 0 || currentBalance < quataDonationAmount){
+                    if(quataDonationAmount < 0 || currentBalance < quataDonationAmount || isNaN(quataDonationAmount)){
                       const modal = document.getElementById('my_modal_3')
                       modal.classList.add('hidden')
-                                        alert('Invalid Amount')
+                                        alert('Invalid Donation Amount')
                                         return
                     }
                   else { document.getElementById('quata-donation-balance').innerText =  quataDonationAmount + quataDonationBalance
                     document.getElementById('current-balance').innerText =  currentBalance - quataDonationAmount
                     const modal = document.getElementById('my_modal_3')
-                    modal.classList.remove('hidden')}
+                    modal.classList.remove('hidden')
+                    const div = document.createElement('div');
+                    div.className = 'border p-3 rounded-lg'
+                    div.innerHTML = `
+                    <p class= "text-gray-500 font-extrabold">${quataDonationAmount} TK Donation for Quata Movement.</p>
+                    <p class= "text-gray-500 font-extrabold">${new Date().toLocaleString()}</p>
+                    `
+                    transectionHistory.appendChild(div)
+                  }
 })
+
+
 
 // history button click
 document.getElementById('history').addEventListener('click',function(){
